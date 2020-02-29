8d3695db0e8fc48f27cab2d342e5767fc74d550e
MhamedEl-shahawy <mohamedelshahawy599@gmail.com>
2020-02-29 17:20:45 +0200

    add new tabs

--- src/dashbouard/main.js
+++ src/dashbouard/main.js
@@ -1,3 +1,12 @@
+let target = document.querySelector('#addnew');
+let availTabel  = document.querySelector('#availabel table tbody');
+let newItems  = document.querySelector('#availabel .new-items');
+let selectType  = document.querySelector('#availabel #type');
+let quantity =  document.querySelector('#availabel .new-items_input');
+let addNew =  document.querySelector('#availabel #addMain');
+
+
+
 /* open tab */
 function openTab(evt, cityName) {
   let i, tabcontent, tablinks,firstTab;
@@ -25,15 +34,7 @@ $(window).load(function() {
 });
 
 
-
-
 /* add new item */
-let target = document.querySelector('#addnew');
-let availTabel  = document.querySelector('#availabel table tbody');
-let newItems  = document.querySelector('#availabel .new-items');
-let selectType  = document.querySelector('#availabel #type');
-let quantity =  document.querySelector('#availabel .new-items_input');
-let addNew =  document.querySelector('#availabel #addMain');
 addNew.addEventListener("click",()=>{
   console.log("me");
     newItems.classList.toggle("show");
@@ -60,22 +61,17 @@ target.addEventListener("click",()=>{
 
    tr.insertAdjacentHTML('beforeend',iconsTd);
    availTabel.appendChild(tr);
+   addTrash();
 });
-
 /* add event remove to all trash icons */
+function addTrash(){
 document.querySelectorAll('.fa-trash-alt').forEach(item => {
   item.addEventListener('click', event => {
   item.offsetParent.parentNode.remove();
   })
 });
+}
+addTrash();
 
- /* 
-  window.onclick= () => {
-  document.querySelectorAll('.fa-trash-alt').forEach(item => {
-  item.addEventListener('click', event => {
-  item.offsetParent.parentNode.remove();
-  })
-})
-};
 
- */
+
