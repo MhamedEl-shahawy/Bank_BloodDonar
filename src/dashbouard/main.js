be5341f7272beb3b77bfc1bdb4dbdf372d4bcecf
MhamedEl-shahawy <mohamedelshahawy599@gmail.com>
2020-03-15 23:07:50 +0200

    return old dashboard

--- src/dashbouard/main.js
+++ src/dashbouard/main.js
@@ -24,6 +24,14 @@ function openTab(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+/* add class main to display block */
+$(window).load(function() {
+    setTimeout(function(){
+        $(".loader").fadeOut("slow");
+        $('.main').css('opacity','1');
+    }, 3000);
+ 
+});
 
 
 /* add new item */
