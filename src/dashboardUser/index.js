21f33a5defb215b1ce0a03a89e1ea460c9d45fa0
FathyMuhamed <fathimuhamed10522@gmail.com>
2020-03-16 18:25:00 +0200

    Add  section donate now

--- src/dashboardUser/index.js
+++ src/dashboardUser/index.js
@@ -4,15 +4,14 @@ const content = document.querySelectorAll('[data-content]');
 link.forEach(clickLink => {
   clickLink.addEventListener('click', () =>{
       const setdata = document.querySelector(clickLink.dataset.target)
-      link.forEach(clickLink =>{
-          clickLink.classList.remove('active');
-      })
       content.forEach(e => {
           e.classList.remove('active');
+      })
+      link.forEach(clickLink =>{
+          clickLink.classList.remove('active');
       })
       clickLink.classList.add('active');
      setdata.classList.add('active');
   })
 });
-
 
