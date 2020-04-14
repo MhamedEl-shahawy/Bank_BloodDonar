1d2c61c71e95071ef1ba104cfac8d1dcff777127
MhamedEl-shahawy <mohamedelshahawy599@gmail.com>
2020-04-15 00:34:55 +0200

    make new dashbourd with sub files

--- src/dashbouard/main.js
+++ src/dashbouard/main.js
@@ -7,24 +7,6 @@ let addNew =  document.querySelectorAll('.addMain');
 
 
 
-/* open tab */
-function openTab(evt, cityName) {
-  let i, tabcontent, tablinks,firstTab;
-  tabcontent = document.getElementsByClassName("tabcontent");
-
-  for (i = 0; i < tabcontent.length; i++) {
-
-    tabcontent[i].style.display = "none";
-
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
 /* add new item */
 addNew.forEach((val)=>{
   val.addEventListener("click",()=>{
