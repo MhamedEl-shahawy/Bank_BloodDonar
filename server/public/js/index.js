a78426bcf763a5fb39d5af3d3c625821a0908d06
FathyMuhamed <fathimuhamed10522@gmail.com>
2020-04-19 01:11:25 +0200

    form edit

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -17,6 +17,23 @@ btnMenu.addEventListener('click', () =>{
     menuShow.classList.toggle('active');
 })
 
+ //counter
+ const counters = document.querySelectorAll('.counter');
+ const speed = 150;
+ counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        const inc = target / speed;
+        if(count < target){
+            counter.innerText = Math.ceil(count + inc) ;
+            setTimeout(updateCount, 1);
+        }else{
+            count.innerText = target;
+        }
+    };
+    updateCount();
+ });
 
  target.forEach(tab => {
      tab.addEventListener('click', () =>{
